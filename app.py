55c86d2fd845b3893730d018b3c37a7c0bcfe02d
Zhuang Zhiyong <zhuangzhiyong@example.com>
2022-07-07 10:03:29 +0800

    try gitignore

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+
+# 设置数据库 URI
+import os
+import sys
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 app = Flask(__name__)
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例app
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
 
 
 @app.route('/')
