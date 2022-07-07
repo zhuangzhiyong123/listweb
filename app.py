ee12d7fdc39fb87e7e6c3ad02d09cf1a0252b691
Zhuang Zhiyong <zhuangzhiyong@example.com>
2022-07-07 10:07:38 +0800

    try again

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     name = db.Column(db.String(20))  # 名字
 
 
+
 class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
