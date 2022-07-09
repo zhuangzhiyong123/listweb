d6069626f1c6ae5b9b0a42c4d3af2888b36e272d
Zhuang Zhiyong <zhuangzhiyong@example.com>
2022-07-09 16:35:38 +0800

    add 用户认证

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import click
+
 from flask import request, url_for, redirect, flash, Flask, render_template
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import login_user
+from flask_login import login_required, logout_user, current_user
 
 # 设置数据库 URI
 import os
@@ -17,9 +24,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例app
 
 
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+class User(db.Model, UserMixin):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字u
+    name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password)  # 将生成的密码保持到对应字段
+
+    def validate_password(self, password):  # 验证密码
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):  # 表名将会是 movie
@@ -37,6 +52,9 @@ def inject_user():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # 判断是否是 POST 请求
+        # 判断是否已登录
+        if not current_user.is_authenticated:
+            redirect(url_for('index'))
         # 获取表单数据
         title = request.form.get('title')  # 传入表单对应输入字段的name值
         if (not title) or (len(title) > 60):
@@ -53,28 +71,30 @@ def index():
 
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
-    if request.method == 'POST': # 处理编辑表单的提交请求
+    if request.method == 'POST':  # 处理编辑表单的提交请求
         title = request.form['title']
         if not title or len(title) > 60:
             flash('Invalid input.')
             return redirect(url_for('edit', movie_id=movie_id))
         # 重定向回对应的编辑页面
-        movie.title = title # 更新标题
-        db.session.commit() # 提交数据库会话
+        movie.title = title  # 更新标题
+        db.session.commit()  # 提交数据库会话
         flash('Item updated.')
-        return redirect(url_for('index')) # 重定向回主页
-    return render_template('edit.html', movie=movie) # 传入被编辑的电影记录
+        return redirect(url_for('index'))  # 重定向回主页
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
 
 
-@app.route('/movie/delete/<int:movie_id>', methods=['POST']) #限定只接受 POST 请求
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+@login_required
 def delete(movie_id):
-    movie = Movie.query.get_or_404(movie_id) # 获取电影记录
-    db.session.delete(movie) # 删除对应的记录
-    db.session.commit() # 提交数据库会话
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
     flash('Item deleted.')
-    return redirect(url_for('index')) # 重定向回主页
+    return redirect(url_for('index'))  # 重定向回主页
 
 
 @app.errorhandler(404)
@@ -92,10 +112,10 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = 'dev'  # 基于安全的考虑，在部署时应该设置为随机字符，且不应该明文写在代码里。
+
 
 # 自定义命令 initdb
-import click
 
 
 @app.cli.command()  # 注册为命令
@@ -138,3 +158,93 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username usedto login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # 设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # 设置密码
+        db.session.add(user)
+
+    db.session.commit()  # 提交数据库会话
+    click.echo('Done.')
+
+
+# 初始化 Flask-Login
+login_manager = LoginManager(app)  # 实例化扩展类
+
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash("Successfully login!")
+            return redirect(url_for('index'))
+        else:
+            flash("Invalid username or password.")  # 若验证失败显示错误信息
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye!')
+    return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if (not username or len(username) > 20) and (not password or len(password) > 128):
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+        # current_user.username = name
+        # current_user 会返回当前登录用户的数据库记录对象
+        # 等同于下面的用法
+        user = User.query.first()
+        user.username = username
+        user.set_password(password)
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
+
+# 如果未登录的用户访问对应的 URL，Flask-Login 会把用户重定向至login_manager.login_view指定的视图端点（函数名）
+login_manager.login_view = 'login'
+# 通过设置 login_manager.login_message 来自定义错误提示消息。
+login_manager.login_message = 'Sorry, please login first.'
